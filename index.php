f5b63d92109a1904889efb4e3b68ac96fbfb498e
Masafumi Noguchi <ngratin@gmail.com>
2017-02-16 15:58:11 +0900

    test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $dsn = 'mysql:host=' . getenv('DB_ADDRESS') . ';dbname=' . getenv('DB_NAME');
 $db_user = getenv('DB_USER');
 $db_password = getenv('DB_PASSWORD');
 
-function connectDb(){
+function connectDb($dsn, $db_user, $db_password){
   try {
     return new PDO($dsn, $db_user, $db_password);
   } catch (PDOException $e) {
@@ -12,5 +12,5 @@ function connectDb(){
   }
 }
 
-connectDb();
+connectDb($dsn, $db_user, $db_password);
 echo "success!";
